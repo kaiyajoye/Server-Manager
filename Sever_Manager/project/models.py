e700a134ee0fe0b95c93acfc3d5049d547dd022c
kaiyajoye <90224390+kaiyajoye@users.noreply.github.com>
2022-12-09 12:19:24 -0700

    Add files via upload

---
+++ Sever_Manager/project/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from user.models import AppUser
+
+# Form for students to create projects
+class Project(models.Model):
+    projecttitle = models.CharField(max_length=50)
+    projectdescription = models.TextField()
+    dockerid = models.CharField(max_length=50)
+    projectlanguage = models.CharField(max_length=50)
+    projectdatabase = models.CharField(max_length=50)
+    projectbackend = models.CharField(max_length=50)
+    projecttechnologymisc = models.CharField(max_length=50)
+    projectlink = models.CharField(max_length=50)
+    projectimagelink = models.CharField(max_length=50)
+    addeddate = models.DateField()
+    updateddate = models.DateField()
+    user = models.ForeignKey(AppUser, on_delete=models.CASCADE)
